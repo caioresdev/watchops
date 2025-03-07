eb8fa7f5dcdad547bd3b62ea114277b575bb010d
Caio Rosa do Espírito Santo <caio.rosa@trustimage.com.br>
2025-03-07 11:31:30 -0300

    feat: send notifications in discord

--- backend/modules/notify.py
+++ backend/modules/notify.py
@@ -1,17 +1,22 @@
 import requests
-import json
-from config import DISCORD_WEBHOOK_URL, DISCORD_NOTIFICATION_ENABLED
+from modules import config
 
-def send_discord_notification(message):
+def send_notification(message: str):
     """
-    Envia uma notificação para o Discord via webhook.
+    Envia uma notificação para o Discord utilizando o webhook configurado.
+    """
+    cfg = config.load_config()
+    webhook_url = cfg.get("webhook", "")
     
-    :param message: Mensagem a ser enviada.
-    :return: Status code da requisição ou None se as notificações estiverem desabilitadas.
-    """
-    if DISCORD_NOTIFICATION_ENABLED:
-        data = {"content": message}
-        headers = {"Content-Type": "application/json"}
-        response = requests.post(DISCORD_WEBHOOK_URL, data=json.dumps(data), headers=headers)
-        return response.status_code
-    return None
+    if not webhook_url:
+        print("Webhook não configurado. Notificação não enviada.")
+        return
+    
+    payload = {"content": message}
+    
+    try:
+        response = requests.post(webhook_url, json=payload)
+        response.raise_for_status()
+        print("Notificação enviada com sucesso!")
+    except Exception as e:
+        print("Erro ao enviar notificação:", e)
